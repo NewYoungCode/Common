1708085e5bc295d9279514829c9c67253752ea8b
ly <ly@DESKTOP-TSUJJL5>
2023-11-09 00:26:52 +0800

    自定义资源文件

--- test/main.cpp
+++ test/main.cpp
@@ -1,8 +1,37 @@
 #include "WebClient.h"
+#include "FileSystem.h"
+#include "Text.h"
 int main() {
-	WebClient wc;
-	Text::Utf8String  resp;
-	wc.HttpGet("www.baidu.com", &resp);
-	printf("%s", resp.ansi().c_str());
+	//WebClient wc;
+	//Text::Utf8String  resp;
+	//wc.HttpGet("www.baidu.com", &resp);
+	//printf("%s", resp.ansi().c_str());
+
+	std::vector<FileSystem::FileInfo> result;
+	FileSystem::Find("d:/test", result, "*.*", true);
+	std::vector<Text::Utf8String> files;
+
+	size_t heads = 0;
+	for (auto& it : result) {
+		if (it.FileType == FileSystem::File) {
+			files.push_back(it.FullName);
+		}
+	}
+
+	unsigned  __int64  headOffset = 0;
+	auto sz = sizeof(__int64);
+
+	File::Delete(L"d:/test.rc");
+	std::ofstream ofs(L"d:/test.rc", std::ios::binary);
+	ofs.seekp(sz);
+	for (auto& it : files) {
+		Text::Utf8String data;
+		File::ReadFile(it, &data);
+		ofs.write(data.c_str(), data.size());
+		headOffset += data.size();
+	}
+	ofs.flush();
+	ofs.close();
+
 	return 0;
 }
