8b96c5feea8888d44d69b07ccaf4982f5947b4f5
yang <yang@DESKTOP-ARS8CM4>
2025-10-02 22:53:25 +0800

    生成代码调整

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 REM 使用默认 VS 版本构建 32 位项目
-cmake -S . -B build -A Win32
+cmake -S . -B build_x86 -A Win32
 pause
--- build64.bat
+++ build64.bat
@@ -1,4 +1,4 @@
 @echo off
 REM 使用默认 VS 版本构建 64 位项目
-cmake -S . -B build64 -A x64
+cmake -S . -B build_x64 -A x64
 pause
