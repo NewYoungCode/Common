c6e78990af9c53de7ecd757f248e7cdd9d318c0f
yang <yang@DESKTOP-CQMRQTC>
2026-04-26 13:53:37 +0800

    更新日志 功能

--- include/Log.h
+++ include/Log.h
@@ -15,15 +15,22 @@ namespace Log {
 	/// <param name="...args"></param>
 	template<typename ...T>
 	inline Text::String Info(const Text::String& formatStr, const T &...args) {
-		// 计算格式化后的字符串所需的内存大小
-		int bufSize = ::snprintf(nullptr, 0, formatStr.c_str(), args...) + 2;  // +1是为了换行符和结束符 \n '\0'
-		char* buf = new char[bufSize];
-		auto count = ::sprintf_s(buf, bufSize, formatStr.c_str(), std::forward<const T&>(args)...);
-		buf[count] = '\n';
-		buf[count + 1] = 0;
-		Text::String info(buf);
+		Text::String info;
+		size_t count = sizeof...(args);
+		if (count > 0) {
+			// 计算格式化后的字符串所需的内存大小
+			int bufSize = ::snprintf(nullptr, 0, formatStr.c_str(), args...) + 2;  // +1是为了换行符和结束符 \n '\0'
+			char* buf = new char[bufSize];
+			auto count = ::sprintf_s(buf, bufSize, formatStr.c_str(), std::forward<const T&>(args)...);
+			buf[count] = '\n';
+			buf[count + 1] = 0;
+			info = buf;
+			delete[] buf;
+		}
+		else {
+			info = formatStr;
+		}
 		info = DateTime::Now().ToString("HH:mm:ss ") + info;
-		delete[] buf;
 		//转为本地可识别的编码
 		auto ansi = info.ansi();
 		std::cout << ansi;
@@ -41,15 +48,22 @@ namespace Log {
 	template<typename ...T>
 	inline void Debug(const Text::String& formatStr, const T &...args) {
 #ifdef  _DEBUG
-		// 计算格式化后的字符串所需的内存大小
-		int bufSize = ::snprintf(nullptr, 0, formatStr.c_str(), args...) + 2;  // +1是为了换行符和结束符 \n '\0'
-		char* buf = new char[bufSize];
-		auto count = ::sprintf_s(buf, bufSize, formatStr.c_str(), std::forward<const T&>(args)...);
-		buf[count] = '\n';
-		buf[count + 1] = 0;
-		Text::String info(buf);
+		Text::String info;
+		size_t count = sizeof...(args);
+		if (count > 0) {
+			// 计算格式化后的字符串所需的内存大小
+			int bufSize = ::snprintf(nullptr, 0, formatStr.c_str(), args...) + 2;  // +1是为了换行符和结束符 \n '\0'
+			char* buf = new char[bufSize];
+			auto count = ::sprintf_s(buf, bufSize, formatStr.c_str(), std::forward<const T&>(args)...);
+			buf[count] = '\n';
+			buf[count + 1] = 0;
+			info = buf;
+			delete[] buf;
+		}
+		else {
+			info = formatStr;
+		}
 		info = DateTime::Now().ToString("HH:mm:ss ") + info;
-		delete[] buf;
 		//转为本地可识别的编码
 		auto ansi = info.ansi();
 		std::cout << ansi;
